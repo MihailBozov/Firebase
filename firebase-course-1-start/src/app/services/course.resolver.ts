03d1cd0cc47c7915be2c119940012eb98eeb4b61
Mihail Bozov <129510773+MihailBozov@users.noreply.github.com>
2025-12-08 20:13:51 +0200

    Load lesons in course component

--- firebase-course-1-start/src/app/services/course.resolver.ts
+++ firebase-course-1-start/src/app/services/course.resolver.ts
@@ -11,10 +11,10 @@ export class CourseResolver implements Resolve<Course> {
     
     constructor(private coursesService: CoursesService){}
     
+    
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
         
         const courseUrl = route.paramMap.get('courseUrl');
-        
          return this.coursesService.findCourseByUrl(courseUrl)
     }
     
--- firebase-course-1-start/src/app/services/courses.service.ts
+++ firebase-course-1-start/src/app/services/courses.service.ts
@@ -5,11 +5,28 @@ import { Course } from "../model/course";
 import { concatMap, map } from "rxjs/operators";
 import { convertSnaps } from "./db-utils";
 import { Lesson } from "../model/lesson";
+import firebase from "firebase"
+
+import OrderByDirection  = firebase.firestore.OrderByDirection;
 
 @Injectable({ providedIn: "root" })
 export class CoursesService {
 
     constructor(private db: AngularFirestore) { }
+
+
+    findLessons(courseId: string, sortOrder: OrderByDirection = 'asc', pageNumber = 0, pageSize = 3): Observable<Lesson[]> {
+
+        return this.db.collection(`courses/${courseId}/lessons`, ref => ref
+            .orderBy('seqNo', sortOrder)
+            .limit(pageSize)
+            .startAfter(pageNumber * pageSize)
+        )
+            .get()
+            .pipe(
+                map(results => convertSnaps<Lesson>(results))
+            )
+    } 
 
 
     findCourseByUrl(courseUrl: string): Observable<Course | null> {
@@ -22,7 +39,6 @@ export class CoursesService {
                 })
             )
     }
-
 
     deleteCoursesAndLessons(courseId: string) {
         return this.db.collection(`courses/${courseId}/lessons`)
