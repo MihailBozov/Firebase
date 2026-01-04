c9ad3ec5dc330310b4d86d5b3e667efb14af3791
Mihail Bozov <129510773+MihailBozov@users.noreply.github.com>
2026-01-04 20:27:23 +0200

    Firebase Storage Security Rules

--- firebase-course-1-start/src/app/app-routing.module.ts
+++ firebase-course-1-start/src/app/app-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
   {
     path: 'create-course',
     component: CreateCourseComponent,
-    //canActivate: [AngularFireAuthGuard],
+    canActivate: [AngularFireAuthGuard],
     data: {
       authGuardPipe: adminOnly
     }
