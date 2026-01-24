b5c28db5db20aab9d8e7445e93ea808a7d902fa2
Mihail Bozov <129510773+MihailBozov@users.noreply.github.com>
2026-01-24 16:57:13 +0200

    Simplifying our promo counter

--- firebase-course-1-start/functions/src/index.ts
+++ firebase-course-1-start/functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 import { db } from "./init";
+import { FieldValue } from "firebase-admin/firestore";
 
 export const onAddCourseUpdatePromoCounter =
   functions.runWith({
@@ -12,13 +13,8 @@ export const onAddCourseUpdatePromoCounter =
 
       const course = snap.data();
       if (course.promo) {
-        return db.runTransaction(async transaction => {
-          const counterRef = db.doc('courses/stats');
-          const snap = await transaction.get(counterRef);
-          const stats = snap.data() ?? { totalPromo: 0 };
-          stats.totalPromo += 1;
-          transaction.set(counterRef, stats);
-          
+        return db.doc("courses/stats").update({
+          totalPromo: FieldValue.increment(1)
         })
       }
     });
