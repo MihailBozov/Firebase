bbc0651e36fef4b348df5f27396490d8d94fc128
Mihail Bozov <129510773+MihailBozov@users.noreply.github.com>
2026-01-25 11:04:36 +0200

    Moving the logic of the promocounter out from the index file and import it dynamically for to save memory

--- firebase-course-1-start/functions/src/index.ts
+++ firebase-course-1-start/functions/src/index.ts
@@ -1,6 +1,4 @@
 import * as functions from "firebase-functions";
-import { db } from "./init";
-import { FieldValue } from "firebase-admin/firestore";
 
 export const onAddCourseUpdatePromoCounter =
   functions.runWith({
@@ -9,12 +7,5 @@ export const onAddCourseUpdatePromoCounter =
   })
     .firestore.document("courses/{courseId}")
     .onCreate(async (snap, context) => {
-      functions.logger.debug(`Running add course trigger for courseId ${context.params.courseId}`);
-
-      const course = snap.data();
-      if (course.promo) {
-        return db.doc("courses/stats").update({
-          totalPromo: FieldValue.increment(1)
-        })
-      }
-    });
+      await (await import("./promotions-counter/on-add-course")).default(snap, context)}
+    );
