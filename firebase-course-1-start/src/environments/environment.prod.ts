9a677d672fd771b47a7317d87af9e008be2814e2
Mihail Bozov <129510773+MihailBozov@users.noreply.github.com>
2026-01-31 20:41:40 +0200

    Production Deployment

--- firebase-course-1-start/src/environments/environment.prod.ts
+++ firebase-course-1-start/src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+    production: true,
+  useEmulators: false,
+  firebase: {
+    apiKey: "AIzaSyCqeLeNb-pxh3eiuOXUv9nnKgpakFQkANk",
+    authDomain: "fir-project-5d64e.firebaseapp.com",
+    projectId: "fir-project-5d64e",
+    storageBucket: "fir-project-5d64e.firebasestorage.app",
+    messagingSenderId: "71531633627",
+    appId: "1:71531633627:web:a3d7df79ade83b54b025fe"
+  },
+  api: {
+    createUser: "",
+  }
 };
